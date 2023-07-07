e6a231adf9aa8d791bbcbd77a3b69a33c635a3b6
jkotania <jkotania14@gmail.com>
2023-07-07 17:54:02 +0200

    Homepage margin

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+// eslint-disable-next-line no-unused-vars
 import { Link } from 'react-router-dom';
 import './Contact.css';
 import Navbar from '../Navbar/Navbar';
